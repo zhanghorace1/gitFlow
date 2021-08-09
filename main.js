2b668f96a0b93bb671e2f51f1585d55ad5ae6675
HoraceZ <zhanghorace@gmail.com>
2021-08-09 11:06:20 -0400

    changes to main, to be added to f1

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-const mainCommit2 = () => {
-  return 'updating main while f1 is working'
-}
-
-const mainCommit3 = () => {
-  return 'second update to main while f1 is working'
-}
-
-const mainCommit4 = () => {
-  return 'another update to main after f1 has rebased and PR into main'
-}
+//Making change to main to be added to f1 later.
