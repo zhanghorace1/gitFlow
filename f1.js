f2f4336c716be144a708e0ef685862066f9fa973
HoraceZ <zhanghorace@gmail.com>
2021-08-09 11:15:11 -0400

    use git pull origin main to merge changes from main into f1

--- f1.js
+++ f1.js
@@ -1,1 +1,4 @@
 //Successfully rebased f1 with main by using 'git rebase main'
+
+//This change is to test 'git pull origin main' instead of rebase to see if I get the same error when attempting to 'git push origin f1'. It looks like using 'git pull origin main' will prompt a commit via VIM.
+
