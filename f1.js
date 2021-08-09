10b29e62dbdced7370eb79493de87f2b24019f11
HoraceZ <zhanghorace@gmail.com>
2021-08-09 11:08:19 -0400

    rebased with main, made changes to be merged to main

--- f1.js
+++ f1.js
@@ -1,11 +1,1 @@
-const feature1Commit1 = () => {
-  return 'this is my first feature commit'
-}
-
-const feature1Commit2 = () => {
-  return 'rebased with main to get all updates'
-}
-
-const feature1Commit3 = () => {
-  return 'updating f1 while behind main'
-}
+//Successfully rebased f1 with main by using 'git rebase main'
